4c0c4a52336b38e70b1b3641d795cf2a211ab9fc
grim <jfalbujas@gmail.com>
2025-03-20 16:08:29 +0100

    more updates

--- userconfig.js
+++ userconfig.js
@@ -12,7 +12,7 @@ const default_config = {
     iconColor: palette.maroon,
   },
   disabled: [],
-  fastlink: "https://nierautomata-anime.com/",
+  //fastlink: "https://nierautomata-anime.com/",
   openLastVisitedTab: true,
   tabs: [
     {
